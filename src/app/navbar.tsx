9c562ce25ad799b1c6fc11cc0a305e99f205d30b
Keith986 <105490335+keith986@users.noreply.github.com>
2025-09-20 12:33:08 +0300

    committed

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link"
 
 export default function Navbar() {
-  return (
-   
+  return (   
 <nav>
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <div></div>
